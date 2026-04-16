68259cf60140fac0ccabc9503d8f8e8f0f0505cd
nguyenhoangphongk18-ux <nguyenhoangphongk18@gmail.com>
2026-04-16 19:50:08 +0700

    Update register.js

--- api/register.js
+++ api/register.js
@@ -1,20 +1,20 @@
-let users = [];
+import { users } from "./data";
 
 export default function handler(req, res) {
   if (req.method === "POST") {
     const { user, pass } = req.body;
 
     if (!user || !pass) {
-      return res.status(200).json({ message: "Thiếu thông tin" });
+      return res.json({ message: "Thiếu thông tin" });
     }
 
     const exists = users.find(u => u.user === user);
     if (exists) {
-      return res.status(200).json({ message: "Tài khoản đã tồn tại" });
+      return res.json({ message: "Tài khoản đã tồn tại" });
     }
 
     users.push({ user, pass });
 
-    res.status(200).json({ message: "Đăng ký thành công" });
+    res.json({ message: "Đăng ký thành công" });
   }
 }
